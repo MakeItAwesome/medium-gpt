07c147cf3a8eb6e5cf033b90d7edafbd1bb2375f
Nicolai Safai <nicolaisafai@gmail.com>
2023-10-15 18:13:27 -0700

    cleanup and add license

--- server.js
+++ server.js
@@ -100,22 +100,27 @@ app.post("/publications/:publicationId/posts", jsonParser, async (req, res) => {
   }
 });
 
-app.get("/logo.png", async (req, res) => {
+app.get("/logo.png", async (_, res) => {
   const filename = path.join(__dirname, "logo.png");
   res.sendFile(filename, { headers: { "Content-Type": "image/png" } });
 });
 
-app.get("/.well-known/ai-plugin.json", async (req, res) => {
-  const host = req.headers.host;
+app.get("/.well-known/ai-plugin.json", async (_, res) => {
   const filePath = path.join(__dirname, ".well-known", "ai-plugin.json");
   const text = await fs.readFile(filePath, "utf8");
   res.set("Content-Type", "application/json").send(text);
 });
 
-app.get("/openapi.yaml", async (req, res) => {
+app.get("/openapi.yaml", async (_, res) => {
   const filePath = path.join(__dirname, "openapi.yaml");
   const text = await fs.readFile(filePath, "utf8");
   res.set("Content-Type", "text/yaml").send(text);
+});
+
+app.get("/license.txt", async (_, res) => {
+  const filePath = path.join(__dirname, "license.txt");
+  const text = await fs.readFile(filePath, "utf8");
+  res.set("Content-Type", "text/plain").send(text);
 });
 
 app.listen(process.env.PORT || port, () => {
